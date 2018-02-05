481b72bf40538904216874c453fd3d68cf3ae4ab
Cole Hansen <cole_hansen@brown.edu>
2018-02-05 09:10:07 -0500

    Additional code

--- js/calendar.js
+++ js/calendar.js
@@ -11,13 +11,30 @@ class Calendar {
         return new Promise(function(resolve, reject) {
             let date = new Date();
             let startDate = date;
-            // startDate.setHours(0);
-            // startDate.setMinutes(0);
-            // startDate.setSeconds(0);
             let endDate = new Date(date);
             endDate.setHours(23);
             endDate.setMinutes(59);
             endDate.setSeconds(59);
+            let path = new URL(self.location + "/calendars/" + self.id + "/events");
+            path.searchParams.append("timeMax", endDate.toISOString());
+            path.searchParams.append("timeMin", startDate.toISOString());
+            path.searchParams.append("singleEvents", "true");
+            gapi.client.request({
+                "path": path.toString()
+            }).then(function(response) {
+                resolve(response.result);
+            },
+            function(reason) {
+                reject(reason);
+            });
+        });
+    }
+
+    eventsUntil(date) {
+        let self = this;
+        return new Promise(function(resolve, reject) {
+            let startDate = new Date();
+            let endDate = date
             let path = new URL(self.location + "/calendars/" + self.id + "/events");
             path.searchParams.append("timeMax", endDate.toISOString());
             path.searchParams.append("timeMin", startDate.toISOString());
--- js/main.js
+++ js/main.js
@@ -1,3 +1,57 @@
+class CalendarDisplay {
+    constructor(calendars, id) {
+        this.calendars = calendars;
+        this.id = id;
+        this.update();
+        window.setInterval(() => this.update(), 1000 * 60 * .1); // 10 second interval to update
+    }
+
+    async update() {
+        let self = this;
+        console.log("update")
+        let events = [];
+    
+        let promises = [];
+        console.log(this.calendars)
+        for (let cal of this.calendars) {
+            promises.push(
+                new Promise(async function(resolve, reject) {
+                    let results = await cal.eventsForToday;
+                    for (let event of results.items) {
+                        events.push({"event": event, "calSummary": results.summary});
+                    }
+                    resolve();
+                })
+            )
+        }
+    
+        let promise = Promise.all(promises);
+    
+        promise.then(function() {
+            events.sort(function(left, right) {
+                let leftTime = new Date(left.event.start.dateTime);
+                let rightTime = new Date(right.event.start.dateTime);
+                return leftTime < rightTime ? -1 : leftTime == rightTime ? 0 : 1;
+            });
+        
+            document.getElementById(self.id).innerHTML = "";
+            if (events.length > 0) {
+                for (let event of events) {
+                    let elTemplate = document.getElementById("calendar-block");
+                    elTemplate.content.querySelectorAll(".time")[0].textContent = (new Date(event.event.start.dateTime).toLocaleTimeString()) + " - " + (new Date(event.event.end.dateTime).toLocaleTimeString());
+                    elTemplate.content.querySelectorAll(".room")[0].textContent = event.calSummary;           
+                    elTemplate.content.querySelectorAll(".summary")[0].textContent = event.event.summary;
+                    let el = document.importNode(elTemplate.content, true);
+                    // el.innerHTML = "(" + new Date(event.event.start.dateTime).toLocaleString() + "): " + (event.calSummary || "No room specified") + " | " + (event.event.summary || "No summary provided.");
+                    document.getElementById(self.id).appendChild(el);
+                }
+            } else {
+                document.getElementById(self.id).innerHTML = "Nothing left for today!";
+            }
+        });    
+    }
+}
+let task;
 function start() {
     // 2. Initialize the JavaScript client library.
     gapi.client.init({
@@ -19,55 +73,57 @@ function start() {
     });
 };
 
-let calendars = [];
-let e;
-
 function init() {
-    calendars.push(new Calendar("https://www.googleapis.com/calendar/v3", "brown.edu_4c4942524f43434f4e46313337313330323037313531333034@resource.calendar.google.com"));
-    calendars.push(new Calendar("https://www.googleapis.com/calendar/v3", "brown.edu_4c494244494744494749313630313630323232313834343038@resource.calendar.google.com"));
-    calendars.push(new Calendar("https://www.googleapis.com/calendar/v3", "brown.edu_4c494244494744494749313533313534313630323232313832353037@resource.calendar.google.com"));
-    calendars.push(new Calendar("https://www.googleapis.com/calendar/v3", "brown.edu_4c494244494744494749313538313630323232313833353433@resource.calendar.google.com"));
-    calendars.push(new Calendar("https://www.googleapis.com/calendar/v3", "brown.edu_4c494244494744494749313535313536313630323232313833303038@resource.calendar.google.com"));
-    calendars.push(new Calendar("https://www.googleapis.com/calendar/v3", "digitalstudiobrown@gmail.com"));
-    update();
-    window.setInterval(update, 1000 * 60 * .1); // 30 minutes interval to update
+    let generalMessagesCalendar = [];
+    let eventCalendars = [];
+    let shiftCalendars = [];
+    eventCalendars.push(new Calendar("https://www.googleapis.com/calendar/v3", "brown.edu_4c4942524f43434f4e46313337313330323037313531333034@resource.calendar.google.com"));
+    eventCalendars.push(new Calendar("https://www.googleapis.com/calendar/v3", "brown.edu_4c494244494744494749313630313630323232313834343038@resource.calendar.google.com"));
+    eventCalendars.push(new Calendar("https://www.googleapis.com/calendar/v3", "brown.edu_4c494244494744494749313533313534313630323232313832353037@resource.calendar.google.com"));
+    eventCalendars.push(new Calendar("https://www.googleapis.com/calendar/v3", "brown.edu_4c494244494744494749313538313630323232313833353433@resource.calendar.google.com"));
+    eventCalendars.push(new Calendar("https://www.googleapis.com/calendar/v3", "brown.edu_4c494244494744494749313535313536313630323232313833303038@resource.calendar.google.com"));
+    eventCalendars.push(new Calendar("https://www.googleapis.com/calendar/v3", "digitalstudiobrown@gmail.com"));
+
+    let generalMessages = new CalendarDisplay(generalMessagesCalendar, "message-cal-display");
+    let todayEvents = new CalendarDisplay(eventCalendars, "cal-display");
+
+    task = new TaskDisplay(eventCalendars, generalMessages, "first-shift-task-display");
 }
 
-async function update() {
-    console.log("update")
-    let events = [];
-
-    let promises = [];
-    for (let cal of calendars) {
-        promises.push(
-            new Promise(async function(resolve, reject) {
-                let results = await cal.eventsForToday;
-                for (let event of results.items) {
-                    events.push({"event": event, "calSummary": results.summary});
-                }
-                resolve();
-            })
-        )
+class TaskDisplay {
+    constructor(roomCalendars, taskCalendars, id) {
+        this.roomCalendars = roomCalendars;
+        this.taskCalendars = taskCalendars;
+        this.id = id;
     }
 
-    let promise = Promise.all(promises);
-
-    promise.then(function() {
-        events.sort(function(left, right) {
-            leftTime = new Date(left.event.start.dateTime);
-            rightTime = new Date(right.event.start.dateTime);
-            return leftTime < rightTime ? -1 : leftTime == rightTime ? 0 : 1;
-        });
-    
-        document.getElementById("cal-display").innerHTML = "";
-        for (let event of events) {
-            let elTemplate = document.getElementById("calendar-block");
-            elTemplate.content.querySelectorAll(".time")[0].textContent = (new Date(event.event.start.dateTime).toLocaleTimeString()) + " - " + (new Date(event.event.end.dateTime).toLocaleTimeString());
-            elTemplate.content.querySelectorAll(".room")[0].textContent = event.calSummary;           
-            elTemplate.content.querySelectorAll(".summary")[0].textContent = event.event.summary;
-            let el = document.importNode(elTemplate.content, true);
-            // el.innerHTML = "(" + new Date(event.event.start.dateTime).toLocaleString() + "): " + (event.calSummary || "No room specified") + " | " + (event.event.summary || "No summary provided.");
-            document.getElementById("cal-display").appendChild(el);
+    get firstShiftTasks() {
+        let shiftEndTime = new Date();
+        if (shiftEndTime.getHours() > 13) {
+            return [];
         }
-    });    
+        shiftEndTime.setHours(13);
+        shiftEndTime.setMinutes(0);
+        shiftEndTime.setSeconds(0);
+
+        let events = [];
+        this.roomCalendars.map(cal => {
+            events.concat(cal.eventsUntil(shiftEndTime));
+        });
+
+        this.taskCalendars.map(cal => {
+            events.concat(
+                cal.eventsUntil(shiftEndTime)
+                .filter(event => {
+                    event.description.startsWith("FIRST:");
+                }
+            ))
+        });
+
+        return events;
+    }
+
+    get lastShiftTasks() {
+
+    }
 }
