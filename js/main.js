78566a9eea8324f570af33ed0bb425c97e4c3053
Kevin Jin <kjin@kevins-mbp-2.devices.brown.edu>
2018-02-23 14:26:38 -0500

    splitting calendars into messages

--- js/main.js
+++ js/main.js
@@ -55,10 +55,10 @@ let task;
 function start() {
     // 2. Initialize the JavaScript client library.
     gapi.client.init({
-        'apiKey': 'AIzaSyBRjtNUliNuV4TMTH06sffXxInW0HLiIro',
+        apiKey: 'AIzaSyBRjtNUliNuV4TMTH06sffXxInW0HLiIro',
         // clientId and scope are optional if auth is not required.
-        'clientId': '158842763808-52v3sccas90s1par8vqdcgmr9d37frql.apps.googleusercontent.com',
-        'scope': 'https://www.googleapis.com/auth/calendar.readonly',
+        clientId: '158842763808-52v3sccas90s1par8vqdcgmr9d37frql.apps.googleusercontent.com',
+        scope: 'https://www.googleapis.com/auth/calendar.readonly',
     }).then(function() {
         if (!gapi.auth2.getAuthInstance().isSignedIn.get()) {
             gapi.auth2.getAuthInstance().signIn().then(function() {
@@ -74,7 +74,7 @@ function start() {
 };
 
 function init() {
-    let generalMessagesCalendar = [];
+    let generalCalendar = [];
     let eventCalendars = [];
     let shiftCalendars = [];
     eventCalendars.push(new Calendar("https://www.googleapis.com/calendar/v3", "brown.edu_4c4942524f43434f4e46313337313330323037313531333034@resource.calendar.google.com"));
@@ -82,9 +82,9 @@ function init() {
     eventCalendars.push(new Calendar("https://www.googleapis.com/calendar/v3", "brown.edu_4c494244494744494749313533313534313630323232313832353037@resource.calendar.google.com"));
     eventCalendars.push(new Calendar("https://www.googleapis.com/calendar/v3", "brown.edu_4c494244494744494749313538313630323232313833353433@resource.calendar.google.com"));
     eventCalendars.push(new Calendar("https://www.googleapis.com/calendar/v3", "brown.edu_4c494244494744494749313535313536313630323232313833303038@resource.calendar.google.com"));
-    eventCalendars.push(new Calendar("https://www.googleapis.com/calendar/v3", "digitalstudiobrown@gmail.com"));
+    generalCalendar.push(new Calendar("https://www.googleapis.com/calendar/v3", "digitalstudiobrown@gmail.com"));
 
-    let generalMessages = new CalendarDisplay(generalMessagesCalendar, "message-cal-display");
+    let generalMessages = new CalendarDisplay(generalCalendar, "message-cal-display");
     let todayEvents = new CalendarDisplay(eventCalendars, "cal-display");
 
     task = new TaskDisplay(eventCalendars, generalMessages, "first-shift-task-display");
