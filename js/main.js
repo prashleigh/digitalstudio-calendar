14c8c02401299d2e267d793e944e6149ae060f24
Kevin Jin <kjin@kevins-mbp-2.devices.brown.edu>
2018-03-01 10:21:15 -0500

    clean up

--- js/main.js
+++ js/main.js
@@ -188,10 +188,10 @@ let roomMap = {}
 function start() {
     // 2. Initialize the JavaScript client library.
     gapi.client.init({
-        apiKey: 'AIzaSyBRjtNUliNuV4TMTH06sffXxInW0HLiIro',
+        apiKey: gapiKey,
         // clientId and scope are optional if auth is not required.
-        clientId: '158842763808-52v3sccas90s1par8vqdcgmr9d37frql.apps.googleusercontent.com',
-        scope: 'https://www.googleapis.com/auth/calendar.readonly',
+        clientId: gapiId,
+        scope: gapiScope,
     }).then(function() {
         if (!gapi.auth2.getAuthInstance().isSignedIn.get()) {
             gapi.auth2.getAuthInstance().signIn().then(function() {
@@ -210,12 +210,12 @@ function init() {
     let generalCalendar = [];
     let eventCalendars = [];
     let shiftCalendars = [];
-    eventCalendars.push(new Calendar("https://www.googleapis.com/calendar/v3", "brown.edu_4c4942524f43434f4e46313337313330323037313531333034@resource.calendar.google.com"));
-    eventCalendars.push(new Calendar("https://www.googleapis.com/calendar/v3", "brown.edu_4c494244494744494749313630313630323232313834343038@resource.calendar.google.com"));
-    eventCalendars.push(new Calendar("https://www.googleapis.com/calendar/v3", "brown.edu_4c494244494744494749313533313534313630323232313832353037@resource.calendar.google.com"));
-    eventCalendars.push(new Calendar("https://www.googleapis.com/calendar/v3", "brown.edu_4c494244494744494749313538313630323232313833353433@resource.calendar.google.com"));
-    eventCalendars.push(new Calendar("https://www.googleapis.com/calendar/v3", "brown.edu_4c494244494744494749313535313536313630323232313833303038@resource.calendar.google.com"));
-    generalCalendar.push(new Calendar("https://www.googleapis.com/calendar/v3", "digitalstudiobrown@gmail.com"));
+    eventCalendars.push(new Calendar("https://www.googleapis.com/calendar/v3", dsl));
+    eventCalendars.push(new Calendar("https://www.googleapis.com/calendar/v3", seminarRoom));
+    eventCalendars.push(new Calendar("https://www.googleapis.com/calendar/v3", avSuite));
+    eventCalendars.push(new Calendar("https://www.googleapis.com/calendar/v3", consultingRoom));
+    eventCalendars.push(new Calendar("https://www.googleapis.com/calendar/v3", digitalStudioRoom));
+    generalCalendar.push(new Calendar("https://www.googleapis.com/calendar/v3", digitalStudioBrown));
 
     roomMap["LIB Rock DSL 137"] = "DSL"
     roomMap["LIB-Digital A/V Suite 153-154"] = "A/V Suite"
